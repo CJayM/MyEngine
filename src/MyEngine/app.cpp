e9bf478816ef8ea6e709b7b1c50a2f5ab595bff0
cjaym <cjaym@yandex.ru>
2020-12-19 05:02:42 +0300

    Ещё повыделял методы (и добавил глобальную переменную приложения)

--- src/MyEngine/app.cpp
+++ src/MyEngine/app.cpp
@@ -1,27 +1,34 @@
 #include "app.h"
 
+#include <cassert>
 #include <stdio.h>
-#include <glad/gl.h>
-#define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
-#include <glad/linmath.h>
 
-#include "shaders_src.h"
+
+
+static App* GLOBAL_APP = nullptr;
 
 void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+    if (GLOBAL_APP == nullptr)
+        return;
+
+    GLOBAL_APP->onError(error, description);
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if (GLOBAL_APP == nullptr)
+        return;
+
+    GLOBAL_APP->onKey(key, scancode, action, mods);
 }
 
 void resize_callback(GLFWwindow* window, int width, int height)
 {
-    fprintf(stderr, "Resize window: %dx%d\n", width, height);
+    if (GLOBAL_APP == nullptr)
+        return;
+
+    GLOBAL_APP->onResize(width, height);
 }
 
 struct Vertex {
@@ -29,36 +36,48 @@ struct Vertex {
     float r, g, b;
 };
 
-
 App::App()
 {
+}
 
+App::~App()
+{
+    if (isInitialized_)
+        glfwTerminate();
 }
 
 void App::init()
 {
+    if (GLOBAL_APP == nullptr)
+        GLOBAL_APP = this;
+
     glfwSetErrorCallback(error_callback);
 
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
+    isInitialized_ = true;
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Simple example", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
+    window_ = glfwCreateWindow(800, 600, "Simple example", NULL, NULL);
+    if (!window_) {
         exit(EXIT_FAILURE);
     }
 
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetFramebufferSizeCallback(window, resize_callback);
+    glfwSetKeyCallback(window_, key_callback);
+    glfwSetFramebufferSizeCallback(window_, resize_callback);
 
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window_);
     gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
+}
 
-    // NOTE: OpenGL error checks have been omitted for brevity
+void App::run()
+{
+    assert(isInitialized_);
+    assert(window_);
 
     Vertex vertices[3] = {
         { -0.6f, -0.4f, 1.f, 0.f, 0.f },
@@ -71,29 +90,17 @@ void App::init()
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &VERTEX_SHADER_SRC, NULL);
-    glCompileShader(vertex_shader);
 
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &FRAGMENT_SHADER_SRC, NULL);
-    glCompileShader(fragment_shader);
-
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    GLint mvp_location = glGetUniformLocation(program, "MVP");
-    GLint vpos_location = glGetAttribLocation(program, "vPos");
-    GLint vcol_location = glGetAttribLocation(program, "vCol");
+    GLint mvp_location = glGetUniformLocation(shaderProgram_, "MVP");
+    GLint vpos_location = glGetAttribLocation(shaderProgram_, "vPos");
+    GLint vcol_location = glGetAttribLocation(shaderProgram_, "vCol");
 
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)0);
+        sizeof(vertices[0]), (void*)0);
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 
     float ratio;
     int width, height;
@@ -102,13 +109,13 @@ void App::init()
     double currentTime = glfwGetTime();
     double lastTime = currentTime;
     double delta = 0;
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window_)) {
         delta = currentTime - lastTime;
         lastTime = currentTime;
 
         glfwPollEvents();
 
-        glfwGetFramebufferSize(window, &width, &height);
+        glfwGetFramebufferSize(window_, &width, &height);
         ratio = width / (float)height;
 
         glViewport(0, 0, width, height);
@@ -119,17 +126,49 @@ void App::init()
         mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         mat4x4_mul(mvp, p, m);
 
-        glUseProgram(program);
+        glUseProgram(shaderProgram_);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window_);
 
         currentTime = glfwGetTime();
     }
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(window_);
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+
+void App::setShaders(const char* vertexShader, const char* fragmentShader)
+{
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertexShader, NULL);
+    glCompileShader(vertex_shader);
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragmentShader, NULL);
+    glCompileShader(fragment_shader);
+
+    shaderProgram_ = glCreateProgram();
+    glAttachShader(shaderProgram_, vertex_shader);
+    glAttachShader(shaderProgram_, fragment_shader);
+    glLinkProgram(shaderProgram_);
+}
+
+void App::onKey(int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window_, GLFW_TRUE);
+}
+
+void App::onResize(int width, int height)
+{
+    fprintf(stderr, "Resize window: %dx%d\n", width, height);
+}
+
+void App::onError(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
 }
--- src/MyEngine/app.h
+++ src/MyEngine/app.h
@@ -1,12 +1,30 @@
 #ifndef APP_H
 #define APP_H
 
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/linmath.h>
 
 class App
 {
 public:
     App();
+    ~App();
     void init();
+    void run();
+    void setShaders(const char* vertexShader, const char* fragmentShader);
+
+    void onKey(int key, int scancode, int action, int mods);
+    void onResize(int width, int height);
+    void onError(int error, const char* description);
+
+protected:
+private:
+    bool isInitialized_ = false;
+    GLFWwindow* window_ = nullptr;
+
+    GLuint shaderProgram_;
 };
 
 #endif // APP_H
