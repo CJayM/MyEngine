c87ee51b3a88eb403701dcf4cfa4d55de6c34395
cjaym <cjaym@yandex.ru>
2020-12-25 01:55:58 +0300

    Подправлена опечатка

--- src/MyEngine/core/models/plane.cpp
+++ src/MyEngine/core/models/plane.cpp
@@ -17,7 +17,7 @@ namespace models {
     std::pair<std::vector<core::Vertex3D>, std::vector<GLuint>> core::models::Plane::makeGeometry()
     {
         float w2 = width * 0.5;
-        float h2 = width * 0.5;
+        float h2 = height * 0.5;
         std::vector<Vertex3D> points = {
             { { w2, h2, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f } },
             { { w2, -h2, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f } },
