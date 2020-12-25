7e4a6e958490168f7c392b529b6dbacaf43aafa0
cjaym <cjaym@yandex.ru>
2020-12-25 03:29:37 +0300

    Добавлено вычисление обратной матрицы

--- src/MyEngine/core/matrices.cpp
+++ src/MyEngine/core/matrices.cpp
@@ -48,24 +48,78 @@ Matrix Matrix::scale(float x, float y)
     return *this;
 }
 
+Matrix Matrix::invert() const
+{
+    float s[6];
+    float c[6];
+    s[0] = data[0][0] * data[1][1] - data[1][0] * data[0][1];
+    s[1] = data[0][0] * data[1][2] - data[1][0] * data[0][2];
+    s[2] = data[0][0] * data[1][3] - data[1][0] * data[0][3];
+    s[3] = data[0][1] * data[1][2] - data[1][1] * data[0][2];
+    s[4] = data[0][1] * data[1][3] - data[1][1] * data[0][3];
+    s[5] = data[0][2] * data[1][3] - data[1][2] * data[0][3];
+
+    c[0] = data[2][0] * data[3][1] - data[3][0] * data[2][1];
+    c[1] = data[2][0] * data[3][2] - data[3][0] * data[2][2];
+    c[2] = data[2][0] * data[3][3] - data[3][0] * data[2][3];
+    c[3] = data[2][1] * data[3][2] - data[3][1] * data[2][2];
+    c[4] = data[2][1] * data[3][3] - data[3][1] * data[2][3];
+    c[5] = data[2][2] * data[3][3] - data[3][2] * data[2][3];
+
+    /* Assumes it is invertible */
+    float idet = 1.0f / (s[0] * c[5] - s[1] * c[4] + s[2] * c[3] + s[3] * c[2] - s[4] * c[1] + s[5] * c[0]);
+
+    Matrix result;
+
+    result.data[0][0] = (data[1][1] * c[5] - data[1][2] * c[4] + data[1][3] * c[3]) * idet;
+    result.data[0][1] = (-data[0][1] * c[5] + data[0][2] * c[4] - data[0][3] * c[3]) * idet;
+    result.data[0][2] = (data[3][1] * s[5] - data[3][2] * s[4] + data[3][3] * s[3]) * idet;
+    result.data[0][3] = (-data[2][1] * s[5] + data[2][2] * s[4] - data[2][3] * s[3]) * idet;
+
+    result.data[1][0] = (-data[1][0] * c[5] + data[1][2] * c[2] - data[1][3] * c[1]) * idet;
+    result.data[1][1] = (data[0][0] * c[5] - data[0][2] * c[2] + data[0][3] * c[1]) * idet;
+    result.data[1][2] = (-data[3][0] * s[5] + data[3][2] * s[2] - data[3][3] * s[1]) * idet;
+    result.data[1][3] = (data[2][0] * s[5] - data[2][2] * s[2] + data[2][3] * s[1]) * idet;
+
+    result.data[2][0] = (data[1][0] * c[4] - data[1][1] * c[2] + data[1][3] * c[0]) * idet;
+    result.data[2][1] = (-data[0][0] * c[4] + data[0][1] * c[2] - data[0][3] * c[0]) * idet;
+    result.data[2][2] = (data[3][0] * s[4] - data[3][1] * s[2] + data[3][3] * s[0]) * idet;
+    result.data[2][3] = (-data[2][0] * s[4] + data[2][1] * s[2] - data[2][3] * s[0]) * idet;
+
+    result.data[3][0] = (-data[1][0] * c[3] + data[1][1] * c[1] - data[1][2] * c[0]) * idet;
+    result.data[3][1] = (data[0][0] * c[3] - data[0][1] * c[1] + data[0][2] * c[0]) * idet;
+    result.data[3][2] = (-data[3][0] * s[3] + data[3][1] * s[1] - data[3][2] * s[0]) * idet;
+    result.data[3][3] = (data[2][0] * s[3] - data[2][1] * s[1] + data[2][2] * s[0]) * idet;
+
+    return result;
+}
+
 Matrix Matrix::ortho(float l, float r, float b, float t, float n, float f)
 {
     Matrix M;
-    M.data[0][0] = 2.f/(r-l);
+    M.data[0][0] = 2.f / (r - l);
     M.data[0][1] = M.data[0][2] = M.data[0][3] = 0.f;
 
-    M.data[1][1] = 2.f/(t-b);
+    M.data[1][1] = 2.f / (t - b);
     M.data[1][0] = M.data[1][2] = M.data[1][3] = 0.f;
 
-    M.data[2][2] = -2.f/(f-n);
+    M.data[2][2] = -2.f / (f - n);
     M.data[2][0] = M.data[2][1] = M.data[2][3] = 0.f;
 
-    M.data[3][0] = -(r+l)/(r-l);
-    M.data[3][1] = -(t+b)/(t-b);
-    M.data[3][2] = -(f+n)/(f-n);
+    M.data[3][0] = -(r + l) / (r - l);
+    M.data[3][1] = -(t + b) / (t - b);
+    M.data[3][2] = -(f + n) / (f - n);
     M.data[3][3] = 1.f;
 
     return M;
+}
+
+Coord2D Matrix::operator*(const Coord2D& vec) const
+{
+    Coord2D result;
+    result.x = data[0][0] * vec.x + data[0][1] * vec.y;
+    result.y = data[1][0] * vec.x + data[1][1] * vec.y;
+    return result;
 }
 
 Matrix operator*(Matrix left, const Matrix& right)
--- src/MyEngine/core/matrices.h
+++ src/MyEngine/core/matrices.h
@@ -1,24 +1,30 @@
 #ifndef CORE_MATRICES_H
 #define CORE_MATRICES_H
 
+#include "core/geom.h"
 #include <array>
 
 namespace core {
 
 struct Matrix {
     Matrix();
-    std::array<std::array<float, 4>, 4> data;
+    static Matrix ortho(float l, float r, float b, float t, float n, float f);
+
 
     void identity();
     Matrix rotateZ(float angle);
     Matrix translate(float x, float y, float z);
     Matrix scale(float x, float y);
-    static Matrix ortho(float l, float r, float b, float t, float n, float f);
+    Matrix invert() const;
 
+    Coord2D operator*(const Coord2D& vec) const;
+
+    std::array<std::array<float, 4>, 4> data;
 };
 
 
 Matrix operator*(Matrix left, const Matrix& right);
+
 
 }
 
